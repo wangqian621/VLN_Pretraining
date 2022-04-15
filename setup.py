37b79150231ab3d04e28e90579f97300850d0747
WANG Qian <98006439+wangqian621@users.noreply.github.com>
2022-04-15 17:09:35 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pkg_resources
 from setuptools import setup, find_packages
 
 setup(
-    name="VLN_Pre",
+    name="VLNPre",
     py_modules=["vlnpre"],
     version="1.0",
     description="",
